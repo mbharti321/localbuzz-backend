d21fe84fd19210f1bc44bbfff089b21d92e161eb
mbharti321 <mbharti321@gmail.com>
2023-07-09 20:09:49 +0530

    add controller,  segregate route defination from router

---
+++ controllers/userController.js
@@ -0,0 +1,36 @@
+// userController.js
+const connection = require("../db_config");
+
+exports.getUsers = (req, res) => {
+  selectQuery = "SELECT * FROM user WHERE id < 5";
+  connection.query(selectQuery, (err, result) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("Internal Server Error");
+    }
+    console.log("GET /user - Success");
+    res.send(result);
+  });
+};
+
+// register user
+/* payload: {
+    "username": "JohnDoe",
+    "password": "password",
+    "email": "mbharti@gmail.com"
+  }
+*/
+exports.registerUser = (req, res) => {
+  const { username, password, email } = req.body;
+  const user = { username, password, email };
+  // console.log(user);
+  const insertQuery = "INSERT INTO user SET ?";
+  connection.query(insertQuery, user, (err, result) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("Internal Server Error");
+    }
+    console.log("POST /user/register - Success");
+    res.status(201).send("User registered successfully");
+  });
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,43 +1,12 @@
 const express = require("express");
 const userRouter = express.Router();
 
-const connection = require("../db_config");
+const userController = require("../controllers/userController");
 
 // get users
-userRouter.get("/", (req, res) => {
-  selectQuery = "SELECT * FROM user WHERE id < 5";
-  connection.query(selectQuery, (err, result) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send("Internal Server Error");
-    }
-    console.log("GET /user - Success");
-    res.send(result);
-  });
-});
-
+userRouter.get("/", userController.getUsers);
 // register user
-/* payload: {
-    "username": "JohnDoe",
-    "password": "password",
-    "email": "mbharti@gmail.com"
-  }
-*/
-userRouter.post("/register", (req, res) => {
-  const { username, password, email } = req.body;
-  const user = { username, password, email };
-  // console.log(user);
-  const insertQuery = "INSERT INTO user SET ?";
-  connection.query(insertQuery, user, (err, result) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send("Internal Server Error");
-    }
-    console.log("POST /user/register - Success");
-    res.status(201).send("User registered successfully");
-  });
-});
-
+userRouter.post("/register", userController.registerUser);
 
 // Export the userRouter
 module.exports = userRouter;
